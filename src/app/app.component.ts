478df546804d2b2d138e2bb6be2ed112e88cae12
Christian Drochon <khrisys@protonmail.com>
2020-07-17 18:15:34 +0200

    Creation et injection du service des users. Ce service contient un tableau d'objet au format json, comme s'il venait d'une API. Il contient egalement des methodes que l'on utilise dans le fichier du composant principal html (app.component.html). On initialise un service dans la methode ngOnInit() du composant principal ts (app.component.ts).
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './services/user.service';
 import {AppareilService} from './services/appareil.service';
 
 @Component({
@@ -6,7 +7,7 @@ import {AppareilService} from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'super appli de fou';
   isAuth: boolean = false;
   // lastUpdate = new Date();
@@ -23,35 +24,39 @@ export class AppComponent {
   appareilTwo = 'Ordinateur';
   appareilThree = 'Nas';
 
-
-  // Array destiné à l'utilisation de *ngFor
-  users = [
-    {
-      name: 'kiki',
-      status: 'codeur'
-    },
-    {
-      name: 'toto',
-      status: 'vip'
-    },
-    {
-      name: 'tata',
-      status: 'farmer'
-    }
-  ];
+  users: any[];
+  appareils: any[];
 
   /**
    * Execute ce code à l'instanciation du composant
    *
-   * @param appareilService : Injection d'une instance du service des appareil dans la constructeur
+   * @param appareilService : Injection d'une instance du service des appareils et des users dans la constructeur
    */
-  constructor(private appareilService: AppareilService) {
+  constructor(private appareilService: AppareilService, private userService: UserService) {
     setTimeout(() => {
       this.isAuth = true;
     }, 4000);
-  };
+  }
+
+  /**
+   * Executé au moment de la creation du component, mais apres l'execution du constructor
+   */
+  ngOnInit(): void {
+    this.users = this.userService.users;
+  }
+  ;
 
   onAllumer() {
     console.log('allumé');
+  }
+
+  // met tous les users à la retraite en appelant la methode retireall() du service des Users
+  retireAll() {
+    this.userService.retireAll();
+  }
+
+  // Met tous les Users comme etant actifs en appelant la methode activeAll() du services des Users
+  activeAll() {
+    this.userService.activeAll();
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,3 +1,38 @@
 export class UserService {
 
+  // Equivalent de la reception d'un tableau d'objet via une API, sous format json
+  // Array utilisé avec *ngFor
+  users = [
+    {
+      name: 'kiki',
+      status: 'retraité'
+    },
+    {
+      name: 'toto',
+      status: 'retraité'
+    },
+    {
+      name: 'tata',
+      status: 'actif'
+    }
+  ];
+
+  /**
+   * Defini tous les users à la retraite
+   */
+  retireAll() {
+    for (let user of this.users) {
+      user.status = 'retraité';
+    }
+  }
+
+  /**
+   * Rend tous les users comme actifs
+   */
+  activeAll() {
+    for (let user of this.users) {
+      user.status = 'actif';
+    }
+  }
+
 }
