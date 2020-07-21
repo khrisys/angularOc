ceec5731309c4431ea5a1c39aca2d8e2eadc52e4
Christian Drochon <khrisys@protonmail.com>
2020-07-21 11:24:09 +0200

    Creation d'un Observable pour implementer un compteur de temps pour savoir depuis combien de temsp on est sur l'appli. On utilise Observable, mais aussi la methode interval() pour incrementer les secondes. Cette methode est accessible en important 'import 'rxjs/add/observable/interval';'. De plus, RxJS ayant été MAJ, il faut aussi installer le package 'rxjs-compat' (via npm), ou alors utiliser les nouvelles syntaxes.
    Dans le composant principal, on implemente le counter en haut à droite de l'ecran dans le fichier html.
    Dans le fichier app.component.ts, c'est là qu'on implemente Observable, et qu'on utilise la methode 'subscribe()'''. Cette methode créée 3 fonctions anonymes : la 1ere attribut la valeur de la var "secondes", la seconde gere toute erreur eventuelle, et la 3e se declenche si l'Observable s'acheve.
    
    Subscriptions :
    Sert à empecher des erreurs sur un Observable personnalisé. On va crée la methode ngOnDestroy() qui va permettre de se declancher lorsqu'on composant est detruit. Ceci evitera les exceptions dû à ce composant.
    
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AppareilService} from './services/appareil.service';
+import {Observable, Subscription} from 'rxjs';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -24,6 +26,10 @@ export class AppComponent implements OnInit {
   appareilTwo = 'Ordinateur';
   appareilThree = 'Nas';
 
+  //Observable
+  secondes: number;
+  counterSubscription: Subscription;
+
 
   /**
    * Le constructeur est executé à l'instanciation du composant
@@ -39,9 +45,35 @@ export class AppComponent implements OnInit {
 
   /**
    * Executé au moment de la creation du component, mais apres l'execution du constructor
-   * Initialise un service
+   *
+   * Dans ce cas, on initialise un observable avec un interval d'une seconde. Il faut importer Observable, mais aussi
+   * 'rxjs/add/observable/interval', et surtout, avec les MAJ qu'il y a eu dans RxJS, il faut installer le package 'rxjs-compat' pour
+   * pouvoir utiliser cette methode 'interval'
+   *
+   * Pour eviter tout pb lorsqu'on utilise des Observable perso, il est vivement conseillé d'utiliser la souscription via l'objet
+   * Subscription
    */
   ngOnInit(): void {
+    const counter = Observable.interval(1000);
+
+    // subscribe() comprend 3  fonctions anonymes, pour gerer les 3 types d'informations que cet Observable peut envoyer
+    this.counterSubscription = counter.subscribe((value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-Oh, an error occured : ' + error);
+      },
+      () => {
+        console.log('Observable complete !');
+      });
+  }
+
+  /**
+   * Methode permettant de detruire la souscription (grace à 'unsubscribe()') et empeche les comportements innatendus liés aux
+   * Observable infinis
+   */
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 
 
