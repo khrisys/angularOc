a57da6316588064359108f3abc9de07b25395412
Christian Drochon <khrisys@protonmail.com>
2020-07-21 21:29:45 +0200

    Validation du form avant submit : - required : attribut Angular stipulant que le champ "name" de l'user doit etre rempli - [disabled] : le bouton est desactivé si le form n'est pas valable sur le bouton - Creation de la var defaultStatus = "Actif" dans le fichier TS, puis dans me html, on lie ce status par defaut avec la directive [ngModel] de l'attribut select, pour etre sur d'avoir un statut lors de la validation du form
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -8,12 +8,19 @@ import {NgForm} from '@angular/forms';
 })
 export class EditUserComponent implements OnInit {
 
+  // le status d'un user par defaut sera à l'etat actif
+  defaultStatus = 'actif';
+
   constructor() {
   }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Methode permettant de recuperer toutes les données du formulaire
+   * @param form : formulaire d'edition des users
+   */
   onSubmit(form: NgForm) {
     console.log(form.value);
   }
