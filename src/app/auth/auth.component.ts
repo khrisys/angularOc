45118148a820180963499602916589ce34964013
Christian Drochon <khrisys@protonmail.com>
2020-07-21 09:50:47 +0200

    Comment
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -11,6 +11,13 @@ export class AuthComponent implements OnInit {
 
   authStatus: boolean;
 
+  /**
+   * Constructeur de l'objet Auth.
+   * Injection de authService et Router
+   *
+   * @param authService : service d'authentification
+   * @param router : permet la redirection vers une page precise apres authentification
+   */
   constructor(private authService: AuthService, private router: Router) {
   }
 
@@ -22,10 +29,12 @@ export class AuthComponent implements OnInit {
    * Comme la methode signIn est async, on utilise le "then()" pour prendre en charge le coté async
    * Le router permet de naviguer vers un ou plusieurs composants precis lors de l'authentification (ici, on va vers le composant des
    * utilisateurs)
+   *
+   * Dans le callback de cette methode,  on redirige l'user vers la page des utilisateurs grace à "navigate()" auquel on passe un array.
    */
   onSignIn() {
     this.authService.signIn().then(() => {
-      console.log('connecté !');
+      // console.log('connecté !');
       this.authStatus = this.authService.isAuth;
       this.router.navigate(['users']);
     });
