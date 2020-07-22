7ad5539edcd1944a5553298bfe780fa2c2fb7b8e
Christian Drochon <khrisys@protonmail.com>
2020-07-22 12:27:47 +0200

    Controls (les champs de formulaire, y compris hobbies de format []) : Comment aouter dynamiquement un ou plusieurs champs sur un form 1. Dans la methode initForm du composant new-user-model, pur l'ajout dynamique des hobbies, on lui ajoute un formBuilder.array([]) vide. 2. Comme il est possible que cet array soit vide parce que l'user n'a pas de hobby, alors dans la methode onSubmitForm(), il faut bien verifier qu'il existe bien des hobbies pour cet user avec une condition ternaire. S'il y en a bien un ou plusieurs, on les ajout dans le formValue, sinon, on met un array vide. 3. Dans le fichier TS du composant, on créé la methode getHobbies() pour des raisons de typage strict, qui retourne le formArray sous forme de formArray 4. Ajout d'un hobby On verifie ce control avec le Validators.required et avec une string vide. Ce qui signifie qu'à partir du moment ou j'ai créé un hobby, le champ devient requis. La methode getHobbies() utilisée ici permet d'avoir acces au formArray. 5. Dans le fichier html, on créé une div pour les hobbies. Ici, la directive n'est pas formControlName mais formArrayName. Ceci pour verifier qu'il s'agit bien d'un array correspondant aux hobbies que j'ai créé dans le fichier TS. On créé une div pour chaque control dans le formArray grace à *ngFor. Il faut aussi recuperer l'index de chasque control à l'interieur du formArray car on veut qu'il y ait un formControlName different pour chaque hobby. Neanmoins, pour utiliser l'index qui s'agit d'une var (et non pas juste de la lettre i), on va lier formControlName à  l'index en mettant entre crochet [formControlName]; On rajoute egalement un bouton, pas de type submit pour ne pas valider l'ensemble du formulaire, pour ajouter un champ text pour un
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

---
+++ src/app/new-user-model/new-user-model.component.ts
@@ -0,0 +1,87 @@
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserModelService} from '../services/user-model.service';
+import {Router} from '@angular/router';
+import {UserModel} from '../models/User.model';
+
+@Component({
+  selector: 'app-new-user-model',
+  templateUrl: './new-user-model.component.html',
+  styleUrls: ['./new-user-model.component.css']
+})
+export class NewUserModelComponent implements OnInit {
+
+  // Object formulaire de type Reactive est de type FormGroup (et non pas ngForm pour la methode template de Angular). Cet objet
+  // correspondra au formulaire dans le template
+  userModelForm: FormGroup;
+
+  /**
+   * @param formBuilder : class Angular qui permet de creer des formulaires plus facilement, des objets de type FormGroup de maniere
+   * plus simple et plus lisible
+   * @param userModelService : injecte le service des UserModel
+   * @param router : permet la navigation vers une page voulue aores traitement des données
+   */
+  constructor(private formBuilder: FormBuilder, private userModelService: UserModelService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  /**
+   * Initialise le formualire. On dit que l'objet de type FormGroup est egal au FormBuilder.group.
+   *
+   * Controls : Dans ce formulaire, on ajoute les controls (les attributs d'un userModel avec valeur par defaut à vides) qui seront dans le
+   * formulaire.
+   * Pour l'ajout dynamique des hobbies, on lui ajoute un formBuilder.array([]) vide.
+   *
+   * Validators : Pour chaque champ du formulaire, on indique avec les Validators qu'il faut que les champs soient remplis , sinon, on ne
+   * pourra passoumettre le le formulaire
+   */
+  initForm() {
+    this.userModelForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      drinkPreferences: ['', Validators.required],
+      hobbies: this.formBuilder.array([])
+    });
+  }
+
+  /**
+   * Soumission du formulaire avec le clic du bouton submit.
+   *
+   * Controls : Il n'y a pas besoin de passer d'argument car la methode reactive de Angular empeche de devoir creer une reference locale.
+   * Pour recuperer les valeurs du formulaire, on créé d'abord une form qui recupere les values = formValue. Puis, on créé un nouvel User
+   * correspondant au formValue ayant tous les attr de cet User. Pour les hobbies, comme  il se peut qu'il n'y en ait pas, il faut
+   * verifier : s'il y en a un ou plusieurs, on les ajout dans le formValue, sinon, on met un array vide.
+   * Ajout des valeurs du form au novel utilisateur
+   * Navigation jusqu'à la page des users
+   */
+  onSubmitForm() {
+    const formValue = this.userModelForm.value;
+    const newModelUser = new UserModel(
+      formValue['firstName'], formValue['lastName'], formValue['email'], formValue['drinkPreferences'], formValue['hobbies'] ? formValue['hobbies'] : []
+    );
+    this.userModelService.addUserModel(newModelUser);
+    this.router.navigate(['/user-model-list']);
+  }
+
+  /**
+   * Pour des raisons de typage strict, on retourne le formArray sous forme de formArray
+   */
+  getHobbies() {
+    return this.userModelForm.get('hobbies') as FormArray;
+  }
+
+  /**
+   * Ajout d'un hobby
+   * On verifie ce control avec le Validators.required et avec une string vide. Ce qui signifie qu'à partir du moment ou j'ai créé un
+   * hobby, le champ devient requis.
+   * La methode getHobbies() utilisée ici permet d'avoir acces au formArray.
+   */
+  onAddHobby() {
+    const newHobbyControl = this.formBuilder.control('', Validators.required);
+    this.getHobbies().push(newHobbyControl);
+  }
+}
