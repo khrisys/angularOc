fa5587323ada1379a50f1ca8d7a2c07188eb9101
Heremiti <heremiti@khris.fr>
2022-03-08 14:16:30 +0100

    correction de fichier de buil du server heroku
    Signed-off-by: Heremiti <heremiti@khris.fr>

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ang-oc1'));
+app.use(express.static(__dirname + '/dist/oc1'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ang-oc1/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/oc1/index.html'));
 });
 app.listen(process.env.PORT || 8080);
